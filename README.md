cc35afe1e117105175e7bb3b8bd4a274ccfd247e
marxjoy <jooy@interia.pl>
2019-07-04 13:42:01 +0200

    + requirements.txt +README.md

---
+++ README.md
@@ -0,0 +1,9 @@
+#snake #python #pygame
+
+Run it: 
+pip3 install -r requirements.txt
+python app.py
+
+
+Snake contol:
+WASD
--- app.py
+++ app.py
@@ -4,62 +4,69 @@ pygame.init()
 a=80
 size = width, height = a*13, a*9
 
-punkty=0
+score = 0
 screenColor = 20, 250, 250
 
 screen = pygame.display.set_mode(size)
+
 
 class Apple:
     x=a
     y=a
     color=250,0,0
+    
     def apear(self):
-        self.x=random.randrange(0,width-a,a)
-        self.y=random.randrange(0,height-a,a)
+        self.x=random.randrange(0, width - a, a)
+        self.y=random.randrange(0, height - a , a)
+        
     def drawApple(self):
-        appleRect = (self.x,self.y,a,a)
-        pygame.draw.rect(screen,self.color, appleRect)
+        appleRect = (self.x, self.y, a, a)
+        pygame.draw.rect(screen, self.color, appleRect)
+
 
 class Snake:
     x=0
     y=0
     speed = [0, 0]
     color = 0, 0, 0
-    colorHead = 100,0,10
+    colorHead = 100, 0, 10
     body=[[x, y]]
+    
     def drawSnake(self):
-        snakeRect = (self.x,self.y,a,a)
-        pygame.draw.rect(screen,self.colorHead, snakeRect)
-        pom=[]
-        new=[self.x, self.y]
+        snakeRect = (self.x, self.y, a, a)
+        pygame.draw.rect(screen, self.colorHead, snakeRect)
+        pom = []
+        new = [self.x, self.y]
         for i in range(0, len(self.body)):
-            pom=self.body[i]
-            self.body[i]=new
-            new=pom
+            pom = self.body[i]
+            self.body[i] = new
+            new = pom
         for i in range(1, len(self.body)):
-            snakeTailRect = (self.body[i][0],self.body[i][1],a,a)
-            pygame.draw.rect(screen,self.color, snakeTailRect)
+            snakeTailRect = (self.body[i][0], self.body[i][1], a, a)
+            pygame.draw.rect(screen, self.color, snakeTailRect)
 
     def vector(self, n):
-        if n==1:
-            self.speed[1]=-a
-            self.speed[0]=0
-        if n==2:
-            self.speed[0]=a
-            self.speed[1]=0
-        if n==3:
-            self.speed[1]=a
-            self.speed[0]=0
-        if n==4:
-            self.speed[0]=-a
-            self.speed[1]=0
+        if n == 1:
+            self.speed[1] =- a
+            self.speed[0] = 0
+        if n == 2:
+            self.speed[0] = a
+            self.speed[1] = 0
+        if n == 3:
+            self.speed[1] = a
+            self.speed[0] = 0
+        if n == 4:
+            self.speed[0] =- a
+            self.speed[1] = 0
+            
     def move(self):
-        self.x+=self.speed[0]
-        self.y+=self.speed[1]
-        new=(self.x,self.y)
+        self.x += self.speed[0]
+        self.y += self.speed[1]
+        new = (self.x,self.y)
 
     def eat(self):
         self.body.append([self.x, self.y])
+
 
 snake = Snake()
 apple= Apple()
@@ -69,10 +76,10 @@ def checkCollision(snake, apple):
     tabX.sort()
     tabY = [snake.y, apple.y]
     tabY.sort()
-    return (tabX[1]-tabX[0] < a) and (tabY[1]-tabY[0] < a)
+    return (tabX[1] - tabX[0] < a) and (tabY[1] - tabY[0] < a)
 
 def checkWallCollision(snake):
-    return (snake.x<=0-a) or (snake.x-a/2>=width) or (snake.y<=0-a) or (snake.y>=height)
+    return (snake.x <= 0-a) or (snake.x-a/2 >= width) or (snake.y <= 0-a) or (snake.y >= height)
 
 def checkTailCollision(snake, tail):
     x=tail[0]
@@ -88,12 +95,14 @@ def gameOver():
 
 gameOver=False
 
+# GAME LOOP
+# 
 while 1:
     screen.fill(screenColor)
     if (checkWallCollision(snake)):
-        gameOver=True
+        gameOver = True
     if (checkCollision(snake,apple)):
-        punkty+=1
+        score += 1
         apple.apear()
         snake.eat()
 
@@ -124,8 +133,8 @@ while 1:
 
     if gameOver:
         while 1:
-            font = pygame.font.SysFont("comicsansms", 72)
-            content="Game Over: SCORE:  " + str(punkty)
+            font = pygame.font.SysFont("comicsansms", 42)
+            content="Game Over: SCORE:  " + str(score)
             text = font.render(content, True, (0, 128, 0))
             screen.blit(text,(320 - text.get_width() // 2, 240 - text.get_height() // 2))
             pygame.display.update()
